8a2a9b2f8dfae6dd9eea9a8eecf199f567feb427
Alex <wndsrfr21@gmail.com>
2019-10-09 00:41:12 -0500

    .

--- main.js
+++ main.js
@@ -16,43 +16,41 @@ canvas.height = window.innerHeight;
 
 setInterval(function () {document.getElementById("myButtonId").click();}, 10000);
 
-function dataURItoBlob(dataURI) {
-  // convert base64/URLEncoded data component to raw binary data held in a string
-  var byteString;
-  if (dataURI.split(',')[0].indexOf('base64') >= 0)
-      byteString = atob(dataURI.split(',')[1]);
-  else
-      byteString = unescape(dataURI.split(',')[1]);
-
-  // separate out the mime component
-  var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
-
-  // write the bytes of the string to a typed array
-  var ia = new Uint8Array(byteString.length);
-  for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
-  }
-
-  return new Blob([ia], {type:mimeString});
-};
-
-
 const button = document.querySelector('button');
 button.onclick = function() {
+
+
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
- 
-    var dataURL = canvas.toDataURL(); //read a single image into dataURL variable
-      var blob = dataURItoBlob(dataURL);
-      var fd = new FormData(document.forms[0]);
-      fd.append("canvasImage",blob);
+    var dataURL = canvas.toDataURL(); //read a single image into dataURL variable    
     var temp = dataURL.replace("data:image/png;base64,", ""); // Verify your base64 data converts correctly to PNG https://onlinepngtools.com/convert-base64-to-png
-    console.log('+++ dataUrl:', temp)
-    var form_data = new FormData(document.forms[0]);
-    form_data.append("image", temp);
-  
-    var xhr = new XMLHttpRequest();
+    
+    
+    //console.log('+++ dataUrl:', temp);
+         
+          var request = new XMLHttpRequest();
+          //Send the proper header information along with the request
+          var formDataA = new FormData(document.forms[0]);
+          var form_data = new FormData(document.forms[0]);
+          formDataA.append('key1', 'value1');
+          formDataA.append('image', temp);
+          form_data.append('image', temp);
+          
+          /*request.open('POST', 'https://httpbin.org/post', false);
+          request.send(formDataA);
+          console.log("data from formDatA...", request.response);
+          request.open('POST', 'https://httpbin.org/post', false);
+          request.send(form_data);
+          console.log("data from form_data...", request.response);*/
+          request.open('POST', 'http://localhost:8080/facebox/check/', false);
+          request.setRequestHeader("Content-Type", "multipart/form-data");
+          //request.setRequestHeader("Accept", "application/json; charset=utf-8");
+          request.send(formDataA);
+          console.log(request.response);
+
+          /*
+    var xhr = new XMLHttpRequest(document.forms[0]);
     xhr.withCredentials = true;
 
     xhr.addEventListener("readystatechange", function () {
@@ -61,10 +59,12 @@ button.onclick = function() {
       }
     });
 
-    xhr.open('POST', '/', true); //http://localhost:8080/facebox/check/
-    xhr.send(form_data);
+    xhr.open('POST', 'https://httpbin.org/post', true); //http://localhost:8080/facebox/check/ https://httpbin.org/post
+    xhr.send();
+    console.log("xhr.response ...",xhr.response);
+     */
 
-    var Boxx = JSON.parse(xhr.response).rects;
+    var Boxx = JSON.parse(response.response).rects;
     console.log(Boxx); 
     var x1 = Boxx[0][0]; 
     var y1 = Boxx[0][1];
